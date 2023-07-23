20fb044cfc8e40d0b7066183096ca1fa9de03678
Hemant Gaur <95062216+HG-IMMORTALS@users.noreply.github.com>
2023-07-23 09:08:18 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Given a linked list of 0s, 1s and 2s, sort it./given-a-linked-list-of-0s-1s-and-2s-sort-it..cpp
@@ -0,0 +1,134 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+/* Link list Node */
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+struct Node *start = NULL;
+
+
+// } Driver Code Ends
+/*
+ 
+  Node is defined as
+  struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+class Solution
+{
+    public:
+    //Function to sort a linked list of 0s, 1s and 2s.
+    Node* segregate(Node *head) {
+         // Add code here
+        int Zeroes = 0;
+        int Ones = 0;
+        int Twos = 0;
+        Node* ptr = head;
+        // Counting total number of Zeroes , Ones and Twos 
+
+        while(ptr != NULL)
+        {
+            if ((ptr -> data) == 0)
+            {
+                Zeroes += 1;
+            }
+            else if ((ptr -> data) == 1)
+            {
+                Ones += 1;
+            }
+            else 
+            {
+                Twos += 1;
+            }
+            ptr = ptr -> next;
+        }
+        ptr = head;
+        while(ptr != NULL){
+            if(Zeroes > 0)
+            {
+                ptr -> data = 0;
+                Zeroes -= 1;
+            }
+            else if(Ones > 0)
+            {
+                ptr -> data = 1;
+                Ones -= 1;
+            }
+            else
+            {
+                ptr -> data = 2;
+                Twos -= 1;
+            }
+            ptr = ptr -> next;
+        }
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Function to sort a linked list of 0s, 1s and 2s
+void printList(struct Node *Node) {
+    while (Node != NULL) {
+        printf("%d ", Node->data);
+        Node = Node->next;
+    }
+    printf("\n");
+}
+
+/* Drier program to test above function*/
+void insert(int n1) {
+    int n, value, i;
+    // scanf("%d",&n);
+    n = n1;
+    struct Node *temp;
+    for (i = 0; i < n; i++) {
+        scanf("%d", &value);
+
+        if (i == 0) {
+            start = new Node(value);
+            temp = start;
+            continue;
+        } else {
+            temp->next = new Node(value);
+            temp = temp->next;
+            temp->next = NULL;
+        }
+    }
+}
+
+int main() {
+
+    int n;
+
+    int t;
+    scanf("%d", &t);
+
+    while (t--) {
+        scanf("%d", &n);
+
+        insert(n);
+        Solution ob;
+        struct Node *newHead = ob.segregate(start);
+        printList(newHead);
+    }
+
+    return 0;
+}
+// } Driver Code Ends
