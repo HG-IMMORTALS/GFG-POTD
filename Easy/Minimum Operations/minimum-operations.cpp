aba4019531eec7fbf30de20caa6bc26d6221ebe8
Hemant Gaur <95062216+HG-IMMORTALS@users.noreply.github.com>
2023-10-26 15:38:31 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Minimum Operations/minimum-operations.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    int minOperation(int n)
+    {
+        vector<int>dp(n+1,-1);
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        
+        for(int i=3;i<=n;i++)
+        {
+            if(i%2==0) dp[i]=min(dp[i-1]+1,dp[i/2]+1);
+            else dp[i]=dp[i-1]+1;
+        }
+        return dp[n];
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	    {
+	        int n;
+	        cin>>n;
+	        Solution ob;
+	        cout<<ob.minOperation(n)<<endl;
+	    }
+}
+// } Driver Code Ends
