a080c6c0ec5d110c5c501b956e61a0787964a0e9
Hemant Gaur <95062216+HG-IMMORTALS@users.noreply.github.com>
2024-02-28 18:19:24 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Check if a number is divisible by 8/check-if-a-number-is-divisible-by-8.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    int DivisibleByEight(string s){
+        int n=s.size();
+        if(n<=3){
+            int x=stoi(s);
+            if(x%8==0) return 1;
+            else return -1;
+        }
+        string str=s.substr(n-3);
+        int num=stoi(str);
+        if(num%8==0) return 1;
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string S;
+        cin>>S;
+        Solution ob;
+        cout<<ob.DivisibleByEight(S)<<"\n";
+    }
+}
+// } Driver Code Ends
