46e7611ca8b9947fdac48a3601d7c658cb3e87c6
Hemant Gaur <95062216+HG-IMMORTALS@users.noreply.github.com>
2024-07-03 08:24:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Remove all occurences of duplicates in a linked list/remove-all-occurences-of-duplicates-in-a-linked-list.cpp
@@ -0,0 +1,103 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Link list node */
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+/* Linked List node structure
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+*/
+
+class Solution {
+  public:
+    Node* removeAllDuplicates(struct Node* head) {
+         map<int,int>mp;
+        Node *temp=head;
+        while(temp!=NULL){
+            mp[temp->data]++;
+            temp=temp->next;
+        }
+        Node *newHead=NULL;
+        for(auto x:mp){
+            if(x.second<=1){
+                if(newHead==NULL){
+                    Node *newNode=new Node(x.first);
+                    newHead=newNode;
+                    head=newHead;
+                }
+                else{
+                    Node *newNode=new Node(x.first);
+                     head->next=newNode;
+                     head=newNode;
+                }
+                
+            }
+        }
+        return newHead;
+    }
+};
+
+//{ Driver Code Starts.
+/* Function to print linked list */
+void printList(struct Node *head) {
+    struct Node *temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+// Driver program to test above functions
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+
+        cin >> N;
+
+        Node *head = NULL;
+        Node *temp = head;
+
+        for (int i = 0; i < N; i++) {
+            int data;
+            cin >> data;
+            if (head == NULL)
+                head = temp = new Node(data);
+            else {
+                temp->next = new Node(data);
+                temp = temp->next;
+            }
+        }
+
+        Solution ob;
+        head = ob.removeAllDuplicates(head);
+        printList(head);
+
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
